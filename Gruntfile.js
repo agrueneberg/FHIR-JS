9c32961185687500e55d01930cf737b926194779
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2014-04-15 14:38:48 -0500

    Rename cgds to fhir.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
         browserify: {
-            cgds: {
+            fhir: {
                 src: "lib/<%= pkg.name %>.js",
                 dest: "dist/<%= pkg.name %>.js",
                 options: {
